efe1ca1eceaff20e3884e5d98da62123ba27a3a2
flashdrag <flashdrag@gmail.com>
2023-04-21 19:20:51 +0100

    Implement delete functionality

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/', views.add_item, name='add'),
     path('edit/<item_id>', views.edit_item, name='edit'),
     path('toogle/<item_id>', views.toogle_item, name='toogle'),
+    path('delete/<item_id>', views.delete_item, name='delete'),
 ]
