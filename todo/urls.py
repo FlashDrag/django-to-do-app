229c406faafaf3b1400716344cbbbe8f7d9ac7d7
flashdrag <flashdrag@gmail.com>
2023-04-21 18:45:10 +0100

    Implement edit functionality

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.get_todo_list, name='get_todo_list'),
     path('add/', views.add_item, name='add'),
+    path('edit/<item_id>', views.edit_item, name='edit')
 ]
