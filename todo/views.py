e8f984a9cf5a772bf48663ee65702c42b1464aac
flashdrag <flashdrag@gmail.com>
2023-04-21 15:56:21 +0100

    Implement displaying tasks

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    # Query the database for all items of the Item model
+    items = Item.objects.all()  # returns a QuerySet
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
